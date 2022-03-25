5505598745b9e19c0ebe15b4df8152a7918d6d75
Bogdan Birau <birau.bogdan92@gmail.com>
2022-03-25 14:57:05 +0100

    Fixed header

--- droid-challenge/src/Components/Home.js
+++ droid-challenge/src/Components/Home.js
@@ -1,5 +1,5 @@
 import { Component } from "react";
-import { Nav } from "react-bootstrap";
+import { Nav, Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { BsSearch } from "react-icons/bs";
 
@@ -7,35 +7,35 @@ class Home extends Component {
   render() {
     return (
       <div className="main_menu">
-        <Nav defaultActiveKey="/home" as="ul" className="bg-white ">
-          <Nav.Item className="mr-auto ">
-            <Nav.Link href="/" className="main_menu_logo">
-              Logo
-            </Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Link to="/">
-              <div className="nav-link ">Home</div>
-            </Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Link to="/about">
-              <div className="nav-link">About</div>
-            </Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Link to="/contact">
-              <div className="nav-link">Contact</div>
-            </Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Link to="/">
-              <div className="nav-link search">
-                <BsSearch />
-              </div>
-            </Link>
-          </Nav.Item>
-        </Nav>
+        <Navbar>
+          <Navbar.Brand className="mr-auto">
+            <Link to="/">Logo</Link>
+          </Navbar.Brand>
+          <Nav defaultActiveKey="/home" as="ul" className="bg-white">
+            <Nav.Item>
+              <Link to="/">
+                <div className="nav-link ">Home</div>
+              </Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Link to="/about">
+                <div className="nav-link">About</div>
+              </Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Link to="/contact">
+                <div className="nav-link">Contact</div>
+              </Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Link to="/">
+                <div className="nav-link search">
+                  <BsSearch />
+                </div>
+              </Link>
+            </Nav.Item>
+          </Nav>
+        </Navbar>
       </div>
     );
   }
